b1cff294ad48ba20809644cac7b03f507667779e
Gilson Filho <contato@gilsondev.com>
2012-04-19 22:42:36 -0300

    Corrigindo configurações do projeto

--- src/settings.py
+++ src/settings.py
@@ -2,16 +2,31 @@
 
 import os
 
-DEBUG = True
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+
+PROJECT_ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-PROJECT_PATH = os.path.dirname(__file__)
-
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('Administrator', 'admin@gmail.com'),
 )
 
 MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+         # Add 'postgresql_psycopg2', 'postgresql', 'mysql',
+         #  'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flisoltweets',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -24,7 +39,7 @@ TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'pt-BR'
 
 SITE_ID = 1
 
@@ -38,31 +53,26 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'public')
+STATIC_ROOT = os.path.join(PROJECT_ROOT_PATH, 'public')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static')
+    os.path.join(PROJECT_ROOT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -74,7 +84,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '!*av32&bysf@&zf2k7=da(96&e!dycny27ol*yj#+j3&0ahvwd'
+SECRET_KEY = '4=ack15a!j(*b!*dmw0#v@a01o-k-so-qjc5*5p$f_a2)yp%(y'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -91,10 +101,14 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'src.urls'
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'django.core.context_processors.request',
+)
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_PATH, 'templates'),
+    os.path.join(PROJECT_ROOT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -104,7 +118,15 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'south',
+
+    #'configurations',
 )
+
+# Auth URLs
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -128,3 +150,9 @@ LOGGING = {
         },
     }
 }
+
+# Override configurations
+try:
+    from settings_local import *
+except ImportError:
+    pass
--- src/settings_local.tpl
+++ src/settings_local.tpl
@@ -2,26 +2,38 @@
 
 import os
 
-from django.conf.global_settings import *
+from settings import *
 
-DEBUG = False
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-PROJECT_PATH = os.path.dirname(__file__)
-
+MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3'
+        # or 'oracle'.
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_PATH, 'flisoltweets.db'),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': os.path.join(PROJECT_DIR, 'flisoltweets.db'),
     }
 }
 
-MIDDLEWARE_CLASSES += ()
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 
-INSTALLED_APPS += ()
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'django_nose',
+)
+
+# Django Debug Toolbar Configurations
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
+# Django Nome
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# NOSE_ARGS = ['--with-notify']
