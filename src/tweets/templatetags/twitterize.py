d46bae64b4be802361a21bd47f3406b49aa1b686
Ivens Rocha <contato@ivens.pro>
2012-04-21 14:36:49 -0300

    data adicionada aos tweets

--- src/tweets/templatetags/twitterize.py
+++ src/tweets/templatetags/twitterize.py
@@ -4,6 +4,11 @@ try:
 except:
     raise Exception('Django is not installed.')
 
+# Ivens Rocha - abril/2012
+# datetime and email needed to make to_datetime work
+from datetime import datetime, timedelta
+from email.utils import parsedate_tz
+
 from twitter_text import TwitterText
 
 register = Library()
@@ -20,3 +25,11 @@ def twitter_text(text, search_query = False):
     tt.autolink.auto_link()
     return tt.text
 twitter_text.is_safe = True
+
+
+@register.filter(name = 'twitter_datetime')
+def to_datetime(datestring):
+    datestring = datestring[:-5] + "+0300"
+    time_tuple = parsedate_tz(datestring.strip())
+    dt = datetime(*time_tuple[:6])
+    return dt - timedelta(seconds=time_tuple[-1])
