d93c9615bc97dd8eaff11f3db1afcee30ea0001e
Gilson Filho <contato@gilsondev.com>
2012-04-22 10:29:22 -0300

    Implementando o acesso a mais tweets

--- src/static/js/tweets.js
+++ src/static/js/tweets.js
@@ -1,51 +1,60 @@
-var ajax = "";
-$(function(){
-    retorna_tweets(1);
-    $('#tweets-box').data('pagina', 1);
-
-});
-
-function inicializa_scroll(){
-    $('#tweets-box').scroll(function() {
-    if(($('#tweets-box').scrollTop() + $('#tweets-box').height() + 400) >= $('#tweets_box').height()) {
-        $('#tweets-box').unbind('scroll');
-        ajax.abort();
-        retorna_tweets($('#tweets-box').data('pagina'));
-    }
-    });
-}
-
+/*
+ * Função que efetua a requisição dos últimos tweets
+ * que usam a hashtag #flisol, com os conteúdos totalmente
+ * formatados (links, usernames, hashtags).
+ *
+ * pagina: número da página a ser pesquisada (padrão: 1)
+ */
 function retorna_tweets(pagina){
-    $('#tweets-box').append('<li class="carregando">Aguarde, carregando...</li>');
-    var url = "/list_ajax";
+    //Oculta o link 'Mais Tweets'
+    $('#carrega-tweets').hide();
 
-    $('#tweets-box').data('pagina', pagina + 1);
+    //Insere a mensagem de carregando
+    $('#tweets-rows').append('<li class="carregando">Aguarde, carregando...</li>');
 
-    ajax = $.ajax({
-        url: url,
+    $.ajax({
+        url: "/list_ajax",
         dataType: 'json',
         data: {page:pagina},
+        timeout: 2000,
+        cache: false,
         success: function(tweets){
             $('.carregando').fadeOut(function(){
-                if(pagina >= 1) {
-                    for(x in tweets) {
-                        $('#tweets-box').append(
-                            '<div class="tweet" id="tweet_'+tweets[x].id_str+'"><div class="tweet-image">'+
-                            '<img src="'+tweets[x].profile_image_url+'" alt="" />'+
-                            '</div>'+
-                            '<div class="tweet-row">'+
-                            '<a href="http://www.twitter.com/"'+tweets[x].from_user+'">'+tweets[x].from_user_name+'</a>'+
-                            '<p class="tweet-text">'+tweets[x].text+'</p>'+
-                            '</div>'+
-                            '</div>'
-                        );
-
-                    }
+                //Iterando os tweets e inserindo em uma div
+                for(x in tweets) {
+                    $('#tweets-rows').append(
+                        '<div class="tweet" id="tweet_'+tweets[x].id_str+'"><div class="tweet-image">'+
+                        '<img src="'+tweets[x].profile_image_url+'" alt="" />'+
+                        '</div>'+
+                        '<div class="tweet-row">'+
+                        '<a href="http://www.twitter.com/'+tweets[x].from_user+'">'+tweets[x].from_user_name+'</a>'+
+                        '<p class="tweet-text">'+tweets[x].text+'</p>'+
+                        '</div>'+
+                        '</div>'
+                    );
                 }
-               $(this).remove();
+
+                // Remove a mensagem de carregando a acrescenta mais um
+                // no atributo data-pagina do link 'Mais Tweets'.
+                $(this).remove();
+                $('#carrega-tweets').data('pagina', pagina + 1).fadeIn();
             });
         },
-        timeout: 5000
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("Ocorreu um erro: "+ errorThrown);
+        }
     });
 }
 
+
+$(function(){
+    retorna_tweets(1);
+    /*
+    * Evento que ao clicar no link 'Mais Tweets' será
+    * requisitado os tweets mais antigos.
+    */
+    $('#carrega-tweets').click(function(e){
+        retorna_tweets($(this).data('pagina'));
+        e.preventDefault();
+    });
+});
