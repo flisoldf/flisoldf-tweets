6f6c8e41f7bbbf959496b021657b4242d0c714e3
Gilson Filho <contato@gilsondev.com>
2012-04-26 13:09:55 -0300

    Registrando os testes da app configurations

---
+++ src/configurations/__init__.py
@@ -0,0 +1,7 @@
+from tests import HashTagsModelTest, HashTagsFormTest, HashTagsViewTest
+
+__test__ = {
+    'HASH_TAG_MODEL_TEST': HashTagsModelTest,
+    'HASH_TAG_FORM_TEST': HashTagsFormTest,
+    'HASH_TAG_VIEW_TEST': HashTagsViewTest
+}
